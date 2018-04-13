14d64baf216069fdd565fd6b4aa4d07d21bf3ee8
nicko858 <ikar2021@hotmail.com>
2018-04-13 13:20:28 +0300

    Скрипт+ readme

--- README.md
+++ README.md
@@ -1,7 +1,66 @@
-# Anti-Duplicator
 
-[TODO. There will be project description]
+
+
+# Anti-Dublicator
+
+The code accept a path to  file-system's directory and find dublicate files.
+
+
+# Quickstart
+
+The program is represented by the module ```dublicates.py```.
+Module ```dublicates.py``` contains the following functions:
+
+- ```get_args()``` - parses script command-line arguments
+- ```get_path_content()``` - accepts the path to file-system directory and returns info about representing files and files-size
+- ```get_dublicate_files()``` - accepts content from ```get_path_content()```-function and returns dublicate files (using file_name  and file-size).
+- ```print_result()``` - prints info about finding files
+
+
+
+The program uses these libs from Python Standart Library:
+
+```python
+argparse
+os
+join, getsize, exists, basename from os.path 
+```
+
+How in works:
+- The program reads  the first command-line argument(path to directory)
+- parsing directory's content using  ```get_path_content()``` -function
+- checking for dublicate files using ``` get_dublicate_files()``` - function
+- prints info about dublicate_files using ```print_result()```-function
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python dublicates.py  <path to dir>
+
+```
+in the console  output you will see something  like this:
+```bash
+In the directory 'C:\Devman\' was found 3 dublicate files:
+
+C:\Devman\5_lang_frequency\bars_new.py
+C:\Devman\3_bars\bars_new.py
+C:\Devman\bars_new.py
+```
+If haven't dublicate files in the directory, you will see such message:
+```The are no dublicate files in the 'C:\Devman\' directory!```
+
+In the cases below, the program will not run:
+
+The program check command-line arguments and if it is wrong,  you will see the warning message ```error: unrecognized arguments``` and usage-message.
+
+If the the directory doesnt't present in the file-system you will see the warning message:
+```No such directory <directory> !```
+
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
+
+
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,64 @@
+import argparse
+import os
+from os.path import join, getsize, exists, basename
+
+
+def get_args():
+    script_usage = 'python dublicates.py  <path to dir>'
+    parser = argparse.ArgumentParser(
+        description='How to run dublicates.py:',
+        usage=script_usage
+    )
+    parser.add_argument(
+        'source_directory',
+        help='Specify the directory you want to check'
+    )
+    args = parser.parse_args()
+    return args
+
+
+def get_dublicate_files(source_path_content):
+    checked_files = {}
+    for source_path, source_file_info in source_path_content.items():
+        if not source_file_info in checked_files.values():
+            checked_files[source_path] = source_file_info
+        else:
+            dublicate_files = []
+            for dubl_path, dubl_file_info in source_path_content.items():
+                if dubl_file_info == source_file_info:
+                    dublicate_files.append(dubl_path)
+            return dublicate_files, len(dublicate_files)
+
+
+def get_path_content(source_path):
+    source_path_content = {}
+    for root, directories, files in os.walk(source_path):
+        for file in files:
+            file_path = join(root, file)
+            file_size = getsize(join(root, file))
+            file_name = basename(file_path)
+            source_path_content[file_path] = [file_name, file_size]
+    return source_path_content
+
+
+def print_result(source_path_content, source_dir):
+    if get_dublicate_files(source_path_content):
+        dublicate_files_list, dublicate_files_count = get_dublicate_files(source_path_content)
+        print('In the directory \'{}\' was found {} dublicate files:\n'.format(source_dir, dublicate_files_count))
+        for files in dublicate_files_list:
+            print(files)
+    else:
+        print('The are no dublicate files in the \'{}\' '
+              'directory!'.format(source_dir))
+
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    try:
+        if exists(args.source_directory):
+            source_path_content = get_path_content(args.source_directory)
+            print_result(source_path_content, args.source_directory)
+        else:
+            raise IOError
+    except IOError:
+        exit('No such directory - {} !').format(args.source_directory)
